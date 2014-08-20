5bc035aaf6a72e24ab4fdbf943e54c907693c693
Marie Hogebrandt <marie@pineberry.com>
2014-08-20 10:55:58 +0200

    Id needs to be a number

--- app/routes.php
+++ app/routes.php
@@ -1,29 +1,32 @@
 <?php
+\Slim\Route::setDefaultConditions(array(
+    'id' => '\d*'
+));
 
 // Find    GET /descriptions/123
 $app->get('/descriptions/:id', function ($id) {
     echo sprintf('Get Description %d', $id);
-});
+})->name('description.find');
 
 // Find All    GET /descriptions
 $app->get('/descriptions', function () {
     echo 'Gets all descriptions based on parameters';
-});
+})->name('description.findAll');
 
 // Update  PUT /descriptions/123
 $app->put('/descriptions/:id', function ($id) {
     echo sprintf('Updates Description %d', $id);
-});
+})->name('description.update');
 
 // Create  POST    /descriptions
 $app->post('/descriptions', function () {
     echo 'Adds Description';
-});
+})->name('description.create');
 
 // Delete  DELETE  /descriptions/123
 $app->delete('/descriptions/:id', function ($id) {
     echo sprintf('Deletes Description %d', $id);
-});
+})->name('description.delete');
 
 
 /*
